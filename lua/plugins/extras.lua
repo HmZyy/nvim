6b0307092d129b5260c6251ce8053dad66da7d74
Hamza ZOGHMAR <ha.zoghmar@gmail.com>
2023-05-13 03:01:25 +0200

    renamed examples.lua to extras.lua

--- lua/plugins/extras.lua
+++ lua/plugins/extras.lua
@@ -1,13 +1,4 @@
--- since this is just an example spec, don't actually load anything here and return an empty spec
--- stylua: ignore
 -- if true then return {} end
-
--- every spec file under config.plugins will be loaded automatically by lazy.nvim
---
--- In your plugin files, you can:
--- * add extra plugins
--- * disable/enabled LazyVim plugins
--- * override the configuration of LazyVim plugins
 return {
   -- add gruvbox
   { "ellisonleao/gruvbox.nvim" },
@@ -229,4 +220,6 @@ return {
       })
     end,
   },
+
+  { "echasnovski/mini.animate", version = false },
 }
