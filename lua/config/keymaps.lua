fbc96d80ef798c6a5ffd2026dc415decc54aeb0a
Hamza ZOGHMAR <ha.zoghmar@gmail.com>
2023-05-13 02:47:17 +0200

    added floating terminal

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,8 +7,14 @@ vim.keymap.set("i", "jk", "<Esc>", { noremap = true, silent = true })
 -- override find files
 vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader><space>", "<cmd>Telescope buffers<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-_>", "<cmd>Telescope find_files<CR>", { noremap = true, silent = true })
 
 -- grep word under the cursor
 vim.keymap.set("n", "ge", "<cmd>Telescope grep_string<CR>", { noremap = true, silent = true })
 vim.keymap.set("v", "ge", "<cmd>Telescope grep_string<CR>", { noremap = true, silent = true })
+
+-- Floating terminal
+vim.keymap.set("n", "<c-\\>", "<cmd>FloatermToggle<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<s-Up>", "<cmd>FloatermNew<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<s-Right>", "<cmd>FloatermNext<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<s-Left>", "<cmd>FloatermPrev<CR>", { noremap = true, silent = true })
