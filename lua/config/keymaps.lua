1917acb1a5d2f94514cb2644b5134162cc9d5daf
Hamza ZOGHMAR <ha.zoghmar@gmail.com>
2023-05-13 12:50:18 +0200

    disabling/chaning default keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,15 +6,38 @@ vim.keymap.set("i", "jk", "<Esc>", { noremap = true, silent = true })
 
 -- override find files
 vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<leader><space>", "<cmd>Telescope buffers<CR>", { noremap = true, silent = true })
+vim.keymap.set(
+  "n",
+  "<leader><space>",
+  "<cmd>Telescope buffers<CR>",
+  { noremap = true, silent = true, desc = "Buffers" }
+)
 vim.keymap.set("n", "<C-_>", "<cmd>Telescope find_files<CR>", { noremap = true, silent = true })
+
+-- override <leader>bb : originally switch to the next buffer
+vim.keymap.set("n", "<leader>bb", function()
+  require("mini.bufremove").delete(0, false)
+end, { noremap = true, silent = true, desc = "Delete buffer" })
+
+vim.keymap.set("n", "<leader>bB", function()
+  require("mini.bufremove").delete(0, true)
+end, { noremap = true, silent = true, desc = "Delete buffer (force)" })
 
 -- grep word under the cursor
 vim.keymap.set("n", "ge", "<cmd>Telescope grep_string<CR>", { noremap = true, silent = true })
 vim.keymap.set("v", "ge", "<cmd>Telescope grep_string<CR>", { noremap = true, silent = true })
+
+-- delete window
+vim.keymap.set("n", "<leader>ww", "<C-W>c", { desc = "Delete Window" })
 
 -- Floating terminal
 vim.keymap.set("n", "<c-\\>", "<cmd>FloatermToggle<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<s-Up>", "<cmd>FloatermNew<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<s-Right>", "<cmd>FloatermNext<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<s-Left>", "<cmd>FloatermPrev<CR>", { noremap = true, silent = true })
+
+-- Remove keymaps
+vim.keymap.set("n", "<leader>`", "", { desc = "Switch to Other Buffer" }) -- <S-H> <S-L>
+vim.keymap.set("n", "<leader>bd", "", { desc = "Delete Buffer" }) -- remapped to <leader>bb
+vim.keymap.set("n", "<leader>bD", "", { desc = "Delete Buffer (Force)" }) -- remapped to <leader>BB
+vim.keymap.set("n", "<leader>wd", "", { desc = "Delete Window" }) -- remapped to <leader>ww
