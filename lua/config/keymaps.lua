cde540f2411edbbbd6491464cc7d4cafd539ff1d
Hamza ZOGHMAR <ha.zoghmar@gmail.com>
2023-05-13 02:08:20 +0200

    grep word under the cursor

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,3 +7,8 @@ vim.keymap.set("i", "jk", "<Esc>", { noremap = true, silent = true })
 -- override find files
 vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader><space>", "<cmd>Telescope buffers<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>", { noremap = true, silent = true })
+
+-- grep word under the cursor
+vim.keymap.set("n", "ge", "<cmd>Telescope grep_string<CR>", { noremap = true, silent = true })
+vim.keymap.set("v", "ge", "<cmd>Telescope grep_string<CR>", { noremap = true, silent = true })
