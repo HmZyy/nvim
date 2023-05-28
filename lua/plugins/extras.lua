5d62fcdc9142d3f6ba90471300e3651f063d835e
Hamza ZOGHMAR <ha.zoghmar@gmail.com>
2023-05-28 13:14:05 +0200

    eslint

--- lua/plugins/extras.lua
+++ lua/plugins/extras.lua
@@ -52,6 +52,26 @@ return {
         -- pyright will be automatically installed with mason and loaded with lspconfig
         pyright = {},
         clangd = {},
+        eslint = {
+          settings = {
+            workingDirectory = { mode = "auto" },
+          },
+        },
+      },
+      setup = {
+        eslint = function()
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            callback = function(event)
+              local client = vim.lsp.get_active_clients({ bufnr = event.buf, name = "eslint" })[1]
+              if client then
+                local diag = vim.diagnostic.get(event.buf, { namespace = vim.lsp.diagnostic.get_namespace(client.id) })
+                if #diag > 0 then
+                  vim.cmd("EslintFixAll")
+                end
+              end
+            end,
+          })
+        end,
       },
     },
   },
