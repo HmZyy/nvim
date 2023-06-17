d71248470db4afa66cc3d5e7ca14f1d476e7dd0e
Hamza ZOGHMAR <ha.zoghmar@gmail.com>
2023-06-17 12:23:44 +0200

    add tokyonight transparency

---
+++ lua/plugins/colorschemes.lua
@@ -0,0 +1,28 @@
+return {
+  -- add gruvbox
+  { "ellisonleao/gruvbox.nvim" },
+
+  -- add catpuccin
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+  },
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
+    },
+  },
+}
--- lua/plugins/extras.lua
+++ lua/plugins/extras.lua
@@ -1,18 +1,5 @@
 -- if true then return {} end
 return {
-  -- add gruvbox
-  -- { "ellisonleao/gruvbox.nvim" },
-  --
-  -- add catpuccin
-  { "catppuccin/nvim", name = "catppuccin" },
-
-  -- Configure LazyVim to load gruvbox
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin",
-    },
-  },
 
   -- change trouble config
   {
