a15648a9a96d2b6dab8ec76e4268cb72247dcd61
Hamza ZOGHMAR <ha.zoghmar@gmail.com>
2023-06-17 12:30:13 +0200

    change lualine theme to bubbles theme

--- lua/plugins/extras.lua
+++ lua/plugins/extras.lua
@@ -119,26 +119,6 @@ return {
     end,
   },
 
-  -- the opts function can also be used to change the default opts:
-  {
-    "nvim-lualine/lualine.nvim",
-    event = "VeryLazy",
-    opts = function(_, opts)
-      table.insert(opts.sections.lualine_x, "😄")
-    end,
-  },
-
-  -- or you can return new options to override all the defaults
-  {
-    "nvim-lualine/lualine.nvim",
-    event = "VeryLazy",
-    opts = function()
-      return {
-        --[[add your custom lualine config here]]
-      }
-    end,
-  },
-
   -- use mini.starter instead of alpha
   -- { import = "lazyvim.plugins.extras.ui.mini-starter" },
 
---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,80 @@
+return {
+  -- the opts function can also be used to change the default opts:
+  -- {
+  --   "nvim-lualine/lualine.nvim",
+  --   event = "VeryLazy",
+  --   opts = function(_, opts)
+  --     table.insert(opts.sections.lualine_x, "😄")
+  --   end,
+  -- },
+
+  -- or you can return new options to override all the defaults
+  {
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    opts = function()
+      -- Bubbles config for lualine
+      -- Author: lokesh-krishna
+      -- MIT license, see LICENSE for more details.
+
+      -- stylua: ignore
+      local colors = {
+        blue   = '#80a0ff',
+        cyan   = '#79dac8',
+        black  = '#080808',
+        white  = '#c6c6c6',
+        red    = '#ff5189',
+        violet = '#d183e8',
+        grey   = '#303030',
+      }
+
+      local bubbles_theme = {
+        normal = {
+          a = { fg = colors.black, bg = colors.violet },
+          b = { fg = colors.white, bg = colors.grey },
+          c = { fg = colors.black, bg = colors.black },
+        },
+
+        insert = { a = { fg = colors.black, bg = colors.blue } },
+        visual = { a = { fg = colors.black, bg = colors.cyan } },
+        replace = { a = { fg = colors.black, bg = colors.red } },
+
+        inactive = {
+          a = { fg = colors.white, bg = colors.black },
+          b = { fg = colors.white, bg = colors.black },
+          c = { fg = colors.black, bg = colors.black },
+        },
+      }
+
+      require("lualine").setup({
+        options = {
+          theme = bubbles_theme,
+          component_separators = "|",
+          section_separators = { left = "", right = "" },
+        },
+        sections = {
+          lualine_a = {
+            { "mode", separator = { left = "" }, right_padding = 2 },
+          },
+          lualine_b = { "filename", "branch" },
+          lualine_c = { "fileformat" },
+          lualine_x = {},
+          lualine_y = { "filetype", "progress" },
+          lualine_z = {
+            { "location", separator = { right = "" }, left_padding = 2 },
+          },
+        },
+        inactive_sections = {
+          lualine_a = { "filename" },
+          lualine_b = {},
+          lualine_c = {},
+          lualine_x = {},
+          lualine_y = {},
+          lualine_z = { "location" },
+        },
+        tabline = {},
+        extensions = {},
+      })
+    end,
+  },
+}
