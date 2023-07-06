2da33b7b36325d61ab72fc7b03a0c186422b741b
Hamza ZOGHMAR <hamza@tilak.io>
2023-07-06 09:35:39 +0200

    add tabnine for windows (NEEDS TESTING)

--- lua/plugins/tabnine.lua
+++ lua/plugins/tabnine.lua
@@ -1,7 +1,15 @@
+local function tabnine_build_path()
+  if vim.loop.os_uname().sysname == "Windows_NT" then
+    return "pwsh.exe -file C:\\Users\\hamza\\AppData\\Local\\nvim-data\\lazy\\tabnine-nvim\\dl_binaries.ps1"
+  else
+    return "/home/hamza/projects/github/tabnine-nvim/dl_binaries.sh"
+  end
+end
+
 return {
   {
     "codota/tabnine-nvim",
-    build = "/home/hamza/projects/github/tabnine-nvim/dl_binaries.sh",
+    build = tabnine_build_path(),
     config = function()
       require("tabnine").setup({
         disable_auto_comment = true,
