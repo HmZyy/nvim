d5158d5f4a50b1cedf7cc02eebb414564230efc1
Hamza ZOGHMAR <hamza@tilak.io>
2023-08-09 14:00:01 +0200

    add tabnine to cmp / remove the previous tabnine plugin

--- lua/plugins/extras.lua
+++ lua/plugins/extras.lua
@@ -19,14 +19,18 @@ return {
     config = true,
   },
 
-  -- override nvim-cmp and add cmp-emoji
+  -- override nvim-cmp and add custom sources
   {
     "hrsh7th/nvim-cmp",
     dependencies = { "hrsh7th/cmp-emoji" },
     ---@param opts cmp.ConfigSchema
     opts = function(_, opts)
       local cmp = require("cmp")
-      opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "emoji" } }))
+      local custom_sources = {
+        { name = "emoji" },
+        { name = "cmp_tabnine" },
+      }
+      opts.sources = cmp.config.sources(vim.list_extend(opts.sources, custom_sources))
     end,
   },
 
--- lua/plugins/tabnine.lua
+++ lua/plugins/tabnine.lua
@@ -8,19 +8,41 @@ end
 
 return {
   {
-    "codota/tabnine-nvim",
-    build = tabnine_build_path(),
+    "tzachar/cmp-tabnine",
+    build = "./install.sh",
+    dependencies = "hrsh7th/nvim-cmp",
     config = function()
-      print(tabnine_build_path())
-      require("tabnine").setup({
-        disable_auto_comment = true,
-        accept_keymap = "<Tab>",
-        dismiss_keymap = "<C-]>",
-        debounce_ms = 800,
-        suggestion_color = { gui = "#808080", cterm = 244 },
-        exclude_filetypes = { "TelescopePrompt" },
-        log_file_path = nil, -- absolute path to Tabnine log file
+      local tabnine = require("cmp_tabnine.config")
+
+      tabnine:setup({
+        max_lines = 1000,
+        max_num_results = 20,
+        sort = true,
+        run_on_every_keystroke = true,
+        snippet_placeholder = "..",
+        ignored_file_types = {
+          -- default is not to ignore
+          -- uncomment to ignore in lua:
+          -- lua = true
+        },
+        show_prediction_strength = false,
       })
     end,
   },
+  -- {
+  --   "codota/tabnine-nvim",
+  --   build = tabnine_build_path(),
+  --   config = function()
+  --     print(tabnine_build_path())
+  --     require("tabnine").setup({
+  --       disable_auto_comment = true,
+  --       accept_keymap = "<Tab>",
+  --       dismiss_keymap = "<C-]>",
+  --       debounce_ms = 800,
+  --       suggestion_color = { gui = "#808080", cterm = 244 },
+  --       exclude_filetypes = { "TelescopePrompt" },
+  --       log_file_path = nil, -- absolute path to Tabnine log file
+  --     })
+  --   end,
+  -- },
 }
