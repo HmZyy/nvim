6d142cf8b12e197fa669bb7d36d105314aebae1f
Hamza ZOGHMAR <hamza@tilak.io>
2023-08-13 14:43:44 +0200

    enable the tabnine plugin / disable the tab/super-tab in cmp

--- lua/plugins/tabnine.lua
+++ lua/plugins/tabnine.lua
@@ -29,20 +29,20 @@ return {
       })
     end,
   },
-  -- {
-  --   "codota/tabnine-nvim",
-  --   build = tabnine_build_path(),
-  --   config = function()
-  --     print(tabnine_build_path())
-  --     require("tabnine").setup({
-  --       disable_auto_comment = true,
-  --       accept_keymap = "<Tab>",
-  --       dismiss_keymap = "<C-]>",
-  --       debounce_ms = 800,
-  --       suggestion_color = { gui = "#808080", cterm = 244 },
-  --       exclude_filetypes = { "TelescopePrompt" },
-  --       log_file_path = nil, -- absolute path to Tabnine log file
-  --     })
-  --   end,
-  -- },
+  {
+    "codota/tabnine-nvim",
+    build = tabnine_build_path(),
+    config = function()
+      print(tabnine_build_path())
+      require("tabnine").setup({
+        disable_auto_comment = true,
+        accept_keymap = "<Tab>",
+        dismiss_keymap = "<C-]>",
+        debounce_ms = 800,
+        suggestion_color = { gui = "#808080", cterm = 244 },
+        exclude_filetypes = { "TelescopePrompt" },
+        log_file_path = nil, -- absolute path to Tabnine log file
+      })
+    end,
+  },
 }
