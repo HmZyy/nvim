a7c33906f727f3cb3e2cbc02cade069b5ffe3e28
Hamza ZOGHMAR <ha.zoghmar@gmail.com>
2024-11-16 22:50:44 +0100

    update gitsign config

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -6,31 +6,41 @@ return {
     config = function()
       require("gitsigns").setup({
         signs = {
-          untracked = { text = "┆" },
-          add = { text = "+" },
-          change = { text = "~" },
+          add = { text = "┃" },
+          change = { text = "┃" },
           delete = { text = "_" },
           topdelete = { text = "‾" },
           changedelete = { text = "~" },
+          untracked = { text = "┆" },
         },
+        signs_staged = {
+          add = { text = "┃" },
+          change = { text = "┃" },
+          delete = { text = "_" },
+          topdelete = { text = "‾" },
+          changedelete = { text = "~" },
+          untracked = { text = "┆" },
+        },
+        signs_staged_enable = true,
         signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
         numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
         linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
         word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
         watch_gitdir = {
-          interval = 1000,
           follow_files = true,
         },
-        -- disable current line blame because git-blame-nvim is smoother
-        attach_to_untracked = true,
+        auto_attach = true,
+        attach_to_untracked = false,
         current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
         current_line_blame_opts = {
           virt_text = true,
           virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
           delay = 1000,
           ignore_whitespace = false,
+          virt_text_priority = 100,
+          use_focus = true,
         },
-        current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
+        current_line_blame_formatter = "<author>, <author_time:%R> - <summary>",
         sign_priority = 6,
         update_debounce = 100,
         status_formatter = nil, -- Use default
@@ -42,9 +52,6 @@ return {
           relative = "cursor",
           row = 0,
           col = 1,
-        },
-        yadm = {
-          enable = false,
         },
       })
     end,
